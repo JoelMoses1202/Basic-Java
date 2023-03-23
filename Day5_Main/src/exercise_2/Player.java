51d238c8ccd8a329ea5ae6941ac7e2d82e256b08
Joel_Moses <joelmoses.sundarrajandevara@cdw.com>
2023-03-23 23:48:23 +0530

    Final_Commit

---
+++ Day5_Main/src/exercise_2/Player.java
@@ -0,0 +1,7 @@
+package exercise_2;
+
+public interface Player {
+  public void assignName(String name);
+  public void mission();
+
+}
---
+++ Day5_Main/src/exercise_3/Exercise_3.java
@@ -0,0 +1,9 @@
+package exercise_3;
+
+public class Exercise_3 {
+	public static void main(String[] args) {
+		Shape circle = new Circle(10, new Blue());
+	    circle.draw();
+	}
+}
+
